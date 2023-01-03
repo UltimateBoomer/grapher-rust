0b28f3ad184838486e513839ddd6ed73c883a77c
UltimateBoomer <caohc2017@gmail.com>
2023-01-03 00:29:02 -0500

    Add `Plotter`

---
+++ src/graph.rs
@@ -0,0 +1,27 @@
+use std::{io::{Write}, error::Error};
+
+use ndarray::{Axis, Array2};
+
+pub struct WritePlotter<W> {
+    writer: W,
+}
+
+impl<W: Write> WritePlotter<W> {
+    pub fn new(writer: W) -> WritePlotter<W> {
+        WritePlotter { writer: writer }
+    }
+
+    pub fn plot_2d(&mut self, graph: Array2<f32>) -> Result<(), Box<dyn Error>> {
+        graph.axis_iter(Axis(0)).map(|a| {
+            let row: String = a.iter().map(|n| {
+                let n = n.abs();
+                if n < 0.01 {"· "}
+                else if n < 0.5 {"* "}
+                else if n < 0.99 {"O "}
+                else {"X "}
+            }).collect();
+            writeln!(&mut self.writer, "{}", row)?;
+            Ok(())
+        }).collect()
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
-use ndarray::{Array2, ArrayBase};
-use num::{Complex, Float, FromPrimitive, Zero};
+pub mod graph;
+
+use ndarray::{Array2, ArrayBase, NdFloat};
+use num::{Complex, Zero};
 use std::{
     error::Error,
-    ops::{AddAssign, DivAssign, MulAssign, RemAssign, SubAssign},
 };
 
-/// This trait allows one to compute values for n-dimensional targets.
-/// By implementing this trait, this takes advantage of Rust's static optimization to achieve zero-cost abstraction. The `apply` function can be inlined to improve efficiency.
-pub trait Draw<T: Float + 'static + FromPrimitive> {
+/// This trait allows one to compute values for n-dimensional targets.\
+/// By implementing this trait, this takes advantage of Rust's static optimization to achieve zero-cost abstraction.\
+/// The `apply` function can be inlined to improve efficiency.
+pub trait Grapher<T: NdFloat> {
     /// Applies the graph function to coordinate `n`.
     /// Inline of possible.
     fn apply(&self, n: &[T]) -> T;
@@ -25,8 +27,8 @@ pub trait Draw<T: Float + 'static + FromPrimitive> {
 
         let mut result = ArrayBase::zeros(size);
         result.indexed_iter_mut().for_each(|((x, y), n)| {
-            let x = T::from_usize(x).unwrap() / T::from_usize(size_x).unwrap() * xr + xa;
-            let y = T::from_usize(y).unwrap() / T::from_usize(size_y).unwrap() * yr + ya;
+            let x = T::from(x).unwrap() / T::from(size_x).unwrap() * xr + xa;
+            let y = T::from(y).unwrap() / T::from(size_y).unwrap() * yr + ya;
             *n = self.apply(&[x, y]);
         });
 
@@ -44,46 +46,45 @@ pub struct DistToGrapher;
 pub struct AddGrapher;
 
 /// This grapher produces the value of the corresponding point in the mandelbrot set.
-pub struct MandelbrotGrapher<T: Float> {
+pub struct MandelbrotGrapher<T: NdFloat> {
     pub iterations: usize,
     pub cutoff: T,
 }
 
-impl<T: Float + 'static + FromPrimitive> Draw<T> for ZeroGrapher {
+impl<T: NdFloat> Grapher<T> for ZeroGrapher {
     #[inline]
     fn apply(&self, _n: &[T]) -> T {
         T::zero()
     }
 }
 
-impl Draw<f32> for DistToGrapher {
+impl Grapher<f32> for DistToGrapher {
     #[inline]
     fn apply(&self, n: &[f32]) -> f32 {
         n.iter().map(|x| x.powi(2)).sum::<f32>().sqrt()
     }
 }
 
-impl Draw<f32> for AddGrapher {
+impl Grapher<f32> for AddGrapher {
     #[inline]
     fn apply(&self, n: &[f32]) -> f32 {
         n.iter().sum()
     }
 }
 
-impl<
-        T: Float + 'static + FromPrimitive + AddAssign + SubAssign + MulAssign + DivAssign + RemAssign,
-    > Draw<T> for MandelbrotGrapher<T>
+impl<T: NdFloat> Grapher<T> for MandelbrotGrapher<T>
 {
     #[inline]
     fn apply(&self, n: &[T]) -> T {
         let mut z = Complex::<T>::zero();
         let c = Complex::new(n[0], n[1]);
         for i in 0..self.iterations {
+            // Don't calculate actual distance to improve performance
             if z.re.abs() > self.cutoff || z.im.abs() > self.cutoff {
-                return T::from(i).unwrap();
+                return T::from(i).unwrap() / T::from(self.iterations).unwrap();
             }
             z += c;
         }
-        T::from(self.iterations).unwrap()
+        T::from(self.iterations).unwrap() / T::from(self.iterations).unwrap()
     }
 }
